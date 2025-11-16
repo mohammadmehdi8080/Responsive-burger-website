1dbe1613962665e650fa82e7abce5c97734b3246
mohammadmehdi <mohammadmehdiz4455@gmail.com>
2025-11-16 16:56:27 +0330

    feat: add home section with styling , responsive design and animations for all section

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,4 +19,23 @@ menu__toggle.addEventListener("click" , ()=>{
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
 
+const sr = ScrollReveal({
+    origin: "top",
+    distance: "60px",
+    duration: 500,
+    delay: 300,
+    reset: true
+})
+
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+sr.reveal(".header__nav__menu",{ origin: 'right', distance: '80px', delay: 300, duration: 1000 })
+sr.reveal(".header__logo" , { origin: 'left', distance: '80px', delay: 300, duration: 1000 })
+sr.reveal(".home__content" , { origin: 'left', distance: '80px', delay: 600, duration: 1500 })
+sr.reveal(".home-dish" , { origin: 'bottom', distance: '80px', delay: 700, duration: 2500 })
+sr.reveal(".home-burger" , { origin: 'top', distance: '100px', delay: 700, duration: 2000 })
+sr.reveal(".home-lettuce-1" , { origin: 'top', distance: '120px', delay: 1000, duration: 2500 })
+sr.reveal(".home-lettuce-2" , { origin: 'top', distance: '120px', delay: 800, duration: 2500 })
+sr.reveal(".home-potato-1" , { origin: 'top', distance: '120px', delay: 1000, duration: 2500 })
+sr.reveal(".home-potato-2" , { origin: 'top', distance: '120px', delay: 800, duration: 2500 })
+sr.reveal(".home-tomato-1" , { origin: 'top', distance: '120px', delay: 1000, duration: 2500 })
+sr.reveal(".home-tomato-2" , { origin: 'top', distance: '120px', delay: 800, duration: 2500 })
